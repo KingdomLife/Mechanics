db6b5db3e4259350a0c92b405909006633f5d784
18PatZ <18patz@students.harker.org>
2016-01-11 08:34:09 -0800

    Changed to Leaping break, no gravity.

--- Mechanics/src/main/java/com/patrickzhong/mechanics/Mechanics.java
+++ Mechanics/src/main/java/com/patrickzhong/mechanics/Mechanics.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
 import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -25,6 +26,8 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
+
+import com.darkblade12.particleeffect.ParticleEffect;
 
 public class Mechanics extends JavaPlugin implements Listener{
 	Plugin plugin;
@@ -80,60 +83,59 @@ public class Mechanics extends JavaPlugin implements Listener{
 	
 	@EventHandler
 	public void onStickClick(PlayerInteractEvent ev){
-		if(ev.getAction().equals(Action.LEFT_CLICK_AIR)){
-			final Player player = ev.getPlayer();
-			ItemStack hand = player.getInventory().getItemInHand();
-			if(hand == null || !hand.getType().equals(Material.STICK) || !hand.hasItemMeta() || !hand.getItemMeta().hasLore())
-				return;
-			List<String> lores = hand.getItemMeta().getLore();
-			for(int a = 0; a < lores.size(); a++){
-				if(ChatColor.stripColor(lores.get(a)).contains("Attack")){
-					ev.setCancelled(true);
-					final Double[] time = new Double[1];
-					final Location[] loc = new Location[1];
-					time[0] = 0.0;
-					final Location location = player.getLocation();
-					loc[0] = new Location(loc[0].getWorld(), location.getX(), location.getY()+0.75, location.getZ());
-					
-					final double yaw = loc[0].getYaw();
-					final double pitch = loc[0].getPitch();
-					final long period = 1;
-					final double range = 10;
-					final double velocity = 20; // Blocks per two seconds
-					timer = new BukkitRunnable(){
-						public void run(){
-							double x = xPos(velocity, yaw, pitch, time[0], loc[0].getX());
-							double y = yPos(velocity, yaw, pitch, time[0], loc[0].getY(), -2);
-							double z = zPos(velocity, yaw, pitch, time[0], loc[0].getZ());
-							Location newLoc = new Location(player.getWorld(), x, y, z);
-							
-							if(!newLoc.getWorld().getBlockAt(newLoc).getType().equals(Material.AIR))
-								this.cancel();
-							
-							for(Entity ent : newLoc.getChunk().getEntities()){
-								if(!ent.equals(player)){
-									Location entLoc = ent.getLocation();
-									boolean closeX = (x <= entLoc.getX()+0.5 && x >= entLoc.getX()-0.5);
-									boolean closeY = (y <= entLoc.getY()+2 && y >= entLoc.getY());
-									boolean closeZ = (z <= entLoc.getZ()+0.5 && z >= entLoc.getZ()-0.5);
-									if(closeX && closeY && closeZ){
-										((Damageable)ent).damage(2);
-										this.cancel();
-									}
+		final Player player = ev.getPlayer();
+		ItemStack hand = player.getInventory().getItemInHand();
+		if(hand == null || !hand.getType().equals(Material.STICK) || !hand.hasItemMeta() || !hand.getItemMeta().hasLore())
+			return;
+		List<String> lores = hand.getItemMeta().getLore();
+		for(int a = 0; a < lores.size(); a++){
+			if(lores.get(a).contains("Attack")){
+				ev.setCancelled(true);
+				final Double[] time = new Double[1];
+				final Location[] loc = new Location[1];
+				time[0] = 0.0;
+				final Location location = player.getLocation();
+				loc[0] = new Location(location.getWorld(), location.getX(), location.getY()+0.75, location.getZ());
+				
+				final double yaw = location.getYaw();
+				final double pitch = location.getPitch();
+				final long period = 1;
+				final double range = 20;
+				final double splash = 1;
+				final double velocity = 20; // Blocks per two seconds
+				timer = new BukkitRunnable(){
+					public void run(){
+						double x = xPos(velocity, yaw, pitch, time[0], loc[0].getX());
+						double y = yPos(velocity, yaw, pitch, time[0], loc[0].getY(), 0);
+						double z = zPos(velocity, yaw, pitch, time[0], loc[0].getZ());
+						Location newLoc = new Location(player.getWorld(), x, y, z);
+						
+						if(!newLoc.getWorld().getBlockAt(newLoc).getType().equals(Material.AIR))
+							this.cancel();
+						
+						for(Entity ent : newLoc.getChunk().getEntities()){
+							if(!ent.equals(player)){
+								Location entLoc = ent.getLocation();
+								boolean closeX = (x <= entLoc.getX()+splash && x >= entLoc.getX()-splash);
+								boolean closeY = (y <= entLoc.getY()+splash*2 && y >= entLoc.getY()-splash/2);
+								boolean closeZ = (z <= entLoc.getZ()+splash && z >= entLoc.getZ()-splash);
+								if(closeX && closeY && closeZ){
+									((Damageable)ent).damage(2);
+									this.cancel();
 								}
 							}
-								
-							newLoc.getWorld().playEffect(newLoc, Effect.POTION_BREAK, 8204);
-							time[0] = time[0] + period/20.0;
-							if(Math.sqrt(Math.pow(location.getX()-x, 2)+Math.pow(location.getZ()-z, 2)) >= range)
-								this.cancel();
 						}
-					}.runTaskTimer(this, 0, period);
-					
-					return;
-				}
+						newLoc.getWorld().playEffect(newLoc, Effect.POTION_BREAK, 8235);
+						time[0] = time[0] + period/20.0;
+						if(Math.sqrt(Math.pow(location.getX()-x, 2)+Math.pow(location.getZ()-z, 2)) >= range)
+							this.cancel();
+					}
+				}.runTaskTimer(this, 0, period);
+				
+				return;
 			}
 		}
+	
 	}
 	
 	private void heal(){
